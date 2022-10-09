1fac1950ce9c91976deaa72642ea6907790ef556
赵宏健 <757566833@qq.com>
2022-10-09 19:33:09 +0800

    feature 添加ref

--- src/components/manage/traits/index.tsx
+++ src/components/manage/traits/index.tsx
@@ -1,8 +1,16 @@
-import React, {useCallback, useEffect, useMemo, useState} from "react";
+import React, {useCallback, useEffect, useImperativeHandle, useMemo, useState} from "react";
 import {Box, Slider, Stack, Typography, Skeleton} from "@mui/material";
 import {getTraits, ITrait} from "@/services";
 import {auto} from "@popperjs/core";
 import {useTraits} from "@/http/trait";
+
+const sum = (arr:number[])=>{
+    let sum = 0;
+    for (const num of arr) {
+        sum+=num
+    }
+    return sum
+}
 
 const map: Record<number, string> = {
     0: 'Zero',
@@ -14,6 +22,12 @@ const map: Record<number, string> = {
 export const Traits: React.FC<{ attributeId: number, total: number }> = (props) => {
     const {attributeId, total} = props;
     const {data, error, isValidating, mutate} =  useTraits(attributeId)
+    const [values,setValues] = useState<number[]>([])
+    useEffect(()=>{
+        if(data){
+            setValues(new Array(data.length).fill(0))
+        }
+    },[data])
     const s = useMemo(() => {
         const array = new Array(total).fill('')
         return array.map((_, index) => <Box key={index} border={'1px solid #dde3e7'} borderRadius={5} width={208}
@@ -40,8 +54,22 @@ export const Traits: React.FC<{ attributeId: number, total: number }> = (props) 
             </Stack>
         </Box>)
     }, [total])
+    const handleChange = useCallback((value:number,index:number)=>{
+        const nextValue = [...values]
+        nextValue[index] = value;
+        setValues(nextValue)
+    },[values])
     const s2 = useMemo(() => {
-        return data?.map((item) => {
+
+        return data?.map((item,index) => {
+            const onChange = (event: Event, value: number | number[], activeThumb: number)=>{
+                handleChange(value as number,index)
+            }
+            const numSum = sum(values);
+            let rate = '0.00'
+            if(numSum!=0){
+                rate = ((values[index] / numSum)*100).toFixed(2)
+            }
             return <Box borderColor={'#dde3e7'} border={'1px solid #dde3e7'} key={item.id} borderRadius={5} width={208}
                         height={328}>
                 <Box height={206} width={206} overflow={"hidden"} borderRadius={5}>
@@ -58,13 +86,18 @@ export const Traits: React.FC<{ attributeId: number, total: number }> = (props) 
                 <Stack width={206} height={120} padding={2} spacing={1}>
                     <Typography fontWeight={"bold"}>{item.name}</Typography>
                     <Box paddingLeft={1} paddingRight={1}>
-                        <Slider marks valueLabelDisplay="auto" step={25} valueLabelFormat={(number) => map[number]}/>
+                        <Slider value={values[index]} onChange={onChange} marks valueLabelDisplay="auto" step={25} valueLabelFormat={(number) => map[number]}/>
                     </Box>
-
+                    <Box paddingLeft={1} paddingRight={1}>
+                        <Typography variant={"body2"}>{rate}%</Typography>
+                    </Box>
                 </Stack>
             </Box>
         })
-    }, [data])
+    }, [data, handleChange, values])
+    // useImperativeHandle(ref,()=>{
+    //
+    // },[])
     return <Stack direction={"row"} width={'100%'} sx={{overflowX:'auto'}} height={360} spacing={3} marginTop={3}>
         {isValidating&&data?.length==0 ? s : s2}
     </Stack>
