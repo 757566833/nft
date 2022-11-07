5799a656ef10c14b936eb01cdb81a1f3a8b7520d
zhj <757566833@qq.com>
2022-11-07 19:54:22 +0800

    release edges

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+    // 因为node_modules/@ethersproject/contracts/src.ts/index.ts:143:5 中有类型错误，所以打包的时候跳过类型检查
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
